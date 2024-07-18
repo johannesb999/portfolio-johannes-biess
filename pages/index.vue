f3d27e3fe0822ee176af23a5a532b818b5bf5b9a
Johannes Biess <128609732+johannesb999@users.noreply.github.com>
2024-07-18 03:25:25 +0200

    split flap effect

--- pages/index.vue
+++ pages/index.vue
@@ -10,20 +10,55 @@
     <div id="projectLink">
       <nuxt-link to="projects" class="custom-link">PROJECTS</nuxt-link>
     </div>
-    <div class="mainText">
-      <div id="line1">HI MY NAME IS</div>
-      <div id="line2">JOHANNES BIESS</div>
-      <div id="line3">AND I AM</div>
-      <div id="line4">DEVELOPER</div>
+    <div class="mainText" @click="animate">
+      <div id="line1">
+        <span v-for="(letter, index) in textArray" :key="index"
+              :class="['char', { 'separator': letter.isSeparator, 'normal': !letter.isSeparator }]">
+          {{ letter.current }}
+        </span>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+
+const originalText = 'HI MY NAME IS';
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ ';
+
+// Hilfsfunktionen
+function getRandomChar() {
+  return alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+
+const textArray = ref(originalText.split('').map(char => ({
+  target: char === ' ' ? getRandomChar() : char,
+  current: getRandomChar(),
+  isSeparator: char === ' ',
+})));
+
+// Animation steuern
+const animate = () => {
+  textArray.value.forEach((item, index) => {
+    const step = () => {
+      if (item.current !== item.target) {
+        item.current = getRandomChar();
+        setTimeout(step, 50);
+      }
+    };
+    setTimeout(step, index * 50);
+  });
+};
+
+onMounted(animate);
+</script>
+
 
 <style scoped>
 #start {
-  color: black;
+  color: #171717;
   font-weight: 300;
   font-size: 2rem;
   text-align: center;
@@ -33,7 +68,7 @@
 }
 
 #projectLink {
-  color: black;
+  color: #171717;
   font-weight: 700;
   font-size: 2rem;
   text-align: center;
@@ -62,13 +97,46 @@
 }
 
 .custom-link {
-  color: black;
+  color: #171717;
   text-decoration: none;
 }
 
 .custom-link:hover {
   color: black;
   text-decoration: underline;
+}
+
+.char {
+  display: inline-block;
+  width: 1.5rem;
+  height: 2rem;
+  overflow: hidden;
+  font-size: 2rem;
+  line-height: 2rem;
+  text-align: center;
+  vertical-align: bottom;
+}
+
+.separator {
+  color: rgb(232, 232, 232);
+}
+
+.normal {
+  color: #171717;
+}
+
+.char.flip {
+  animation: flap 1s ease-in-out forwards;
+}
+
+@keyframes flap {
+  0%,
+  100% {
+    transform: rotateX(0deg);
+  }
+  50% {
+    transform: rotateX(360deg);
+  }
 }
 
 .mainText {
